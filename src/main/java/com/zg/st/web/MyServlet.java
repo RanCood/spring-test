5a2c842b07b0782289b9da29eb713e72f98b633d
zg <zhanggang5>
2020-06-10 10:22:07 +0800

    init

---
+++ src/main/java/com/zg/st/web/MyServlet.java
@@ -0,0 +1,25 @@
+package com.zg.st.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author: zg
+ * @date: 2020/6/9 18:51
+ */
+@WebServlet(urlPatterns = "/")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+        pw.write("<h1>Hello, world!</h1>");
+        pw.flush();
+    }
+}
